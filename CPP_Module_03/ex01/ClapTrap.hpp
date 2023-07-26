38709f1c364a942bc713b8c6f52e5674678e0f1b
Milimas <a.x.man@hotmail.fr>
2023-07-26 14:20:27 +0100

    copy constructor without calling operator=

--- CPP_Module_03/ex01/ClapTrap.hpp
+++ CPP_Module_03/ex01/ClapTrap.hpp
@@ -5,31 +5,32 @@
 
 class ClapTrap
 {
-private:
+protected:
     std::string name ;
     int    hitPoint ;       //10
     int    energyPoint ;    //10
     int    attackDamage ;   //0
-
 public:
     ClapTrap( void ) ;
-    ClapTrap( std::string name ) ;
-    ClapTrap( std::string name, int hitPoint, int energyPoint, int attackDamage ) ;
+    ClapTrap( const ClapTrap& clapTrap ) ;
+    ClapTrap& operator=( const ClapTrap& other ) ;
     ~ClapTrap( void ) ;
+    
+    ClapTrap( std::string name ) ;
 
     void attack( const std::string& target ) ;
     void takeDamage( unsigned int amount ) ;
     void beRepaired( unsigned int amount ) ;
 
     std::string getName() ;
-    int getHitPoint() ;
-    int getEnergyPoint() ;
+    // int getHitPoint() ;
+    // int getEnergyPoint() ;
     int getAttackDamage() ;
 
-    void setName( std::string name ) ;
-    void setHitPoint( int amount ) ;
-    void setEnergyPoint( int amount ) ;
-    void setAttackDamage( int amount ) ;
+    // void setName( std::string name ) ;
+    // void setHitPoint( int amount ) ;
+    // void setEnergyPoint( int amount ) ;
+    // void setAttackDamage( int amount ) ;
 
 };
 
