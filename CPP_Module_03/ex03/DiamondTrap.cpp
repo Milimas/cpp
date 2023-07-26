8fc2827e0837e583a2bb7ea9e593ec770c58236c
Milimas <a.x.man@hotmail.fr>
2023-07-26 17:14:57 +0100

    call operator= for base classes new whoAmI and attack implementation

--- CPP_Module_03/ex03/DiamondTrap.cpp
+++ CPP_Module_03/ex03/DiamondTrap.cpp
@@ -1,30 +1,75 @@
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap( void ): ClapTrap("DiamondTrap_clap_name"), ScavTrap("DiamondTrap_scav_name"), FragTrap("DiamondTrap_frag_name")
+DiamondTrap::DiamondTrap( void ): ClapTrap("DiamondTrap_clap_name")
 {
-    this->name = "DiamondTrap" ;
-    ClapTrap::name = name + "_clap_name" ;
-    this->energyPoint = 50 ; 
-    std::cout << this->name << " aka " << ClapTrap::name << " Spawned!" << std::endl ;
+    this->_name = "DiamondTrap" ;
+    ClapTrap::name = _name + "_clap_name" ;
+    ClapTrap::hitPoint = FragTrap::_hitPoint ;
+    ClapTrap::energyPoint = ScavTrap::energyPoint ;
+    ClapTrap::attackDamage = FragTrap::_attackDamage ;
+    // std::cout << this->_name << " aka " << ClapTrap::name << " Spawned!" << std::endl ;
 }
 
-DiamondTrap::DiamondTrap( std::string name ): ClapTrap(name + "_clap_name"), ScavTrap(name + "_scav_name"), FragTrap(name + "_frag_name")
+DiamondTrap::DiamondTrap( const DiamondTrap& other ): ClapTrap(other.name + "_clap_name")
 {
-    this->name = name ;
+    this->_name = other.name ;
+    ClapTrap::operator=((ClapTrap)other) ;
+    FragTrap::operator=((FragTrap)other) ;
+    ScavTrap::operator=((ScavTrap)other) ;
+    // std::cout << this->_name << " aka " << ClapTrap::name << " Spawned!" << std::endl ;
+}
+
+DiamondTrap& DiamondTrap::operator=( const DiamondTrap& other )
+{
+    std::cout << this->_name << " aka " << ClapTrap::name << " Changed to " << other.name << "!" << std::endl ;
+    if (this != &other)
+    {
+        this->_name = other.name ;
+        ClapTrap::operator=((ClapTrap)other) ;
+        FragTrap::operator=((FragTrap)other) ;
+        ScavTrap::operator=((ScavTrap)other) ;
+    }
+    return (*this) ;
+}
+
+DiamondTrap::DiamondTrap( const std::string& name ): ClapTrap(name + "_clap_name")
+{
+    this->_name = name ;
     ClapTrap::name = name + "_clap_name" ;
-    this->energyPoint = 50 ;
-    std::cout << this->name << " aka " << ClapTrap::name << " Spawned!" << std::endl ;
+    ClapTrap::hitPoint = FragTrap::_hitPoint ;
+    ClapTrap::energyPoint = ScavTrap::_energyPoint ;
+    ClapTrap::attackDamage = FragTrap::_attackDamage ;
+    // std::cout << this->_name << " aka " << ClapTrap::name << " Spawned!" << std::endl ;
 }
 
 DiamondTrap::~DiamondTrap( void )
 {
-    std::cout << this->name << " aka " << ClapTrap::name << " Died!" << std::endl ;
+    std::cout << this->_name << " aka " << ClapTrap::name << " Died!" << std::endl ;
 }
 
 void DiamondTrap::whoAmI()
 {
-    std::cout << "I'm " << this->name << " and my clap name is " << ClapTrap::name << std::endl ;
-    std::cout << "hitPoint: " << ClapTrap::hitPoint << std::endl ;
-    std::cout << "energyPoint: " << ClapTrap::energyPoint << std::endl ;
-    std::cout << "attackDamage: " << ClapTrap::attackDamage << std::endl ;
+    std::cout << "-------------------------------------------------" << std::endl ;
+    std::cout << "Diamond::_name:            " << this->_name << std::endl ;
+    std::cout << "-------------------------------------------------" << std::endl ;
+    std::cout << "ClapTrap::name:            " << ClapTrap::name << std::endl ;
+    std::cout << "ClapTrap::hitPoint:        " << ClapTrap::hitPoint << std::endl ;
+    std::cout << "ClapTrap::energyPoint:     " << ClapTrap::energyPoint << std::endl ;
+    std::cout << "ClapTrap::attackDamage:    " << ClapTrap::attackDamage << std::endl ;
+    std::cout << "-------------------------------------------------" << std::endl ;
+    std::cout << "ScavTrap::_name:           " << ScavTrap::_name << std::endl ;
+    std::cout << "ScavTrap::_hitPoint:       " << ScavTrap::_hitPoint << std::endl ;
+    std::cout << "ScavTrap::_energyPoint:    " << ScavTrap::_energyPoint << std::endl ;
+    std::cout << "ScavTrap::_attackDamage:   " << ScavTrap::_attackDamage << std::endl ;
+    std::cout << "-------------------------------------------------" << std::endl ;
+    std::cout << "FragTrap::_name:           " << FragTrap::_name << std::endl ;
+    std::cout << "FragTrap::_hitPoint:       " << FragTrap::_hitPoint << std::endl ;
+    std::cout << "FragTrap::_energyPoint:    " << FragTrap::_energyPoint << std::endl ;
+    std::cout << "FragTrap::_attackDamage:   " << FragTrap::_attackDamage << std::endl ;
+    std::cout << "-------------------------------------------------" << std::endl ;
+}
+
+void DiamondTrap::attack( const std::string& target )
+{
+    ScavTrap::attack(target) ;
 }
