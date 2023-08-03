9bac6549b784b59107ca28beb02a9f31fe4e2f9e
Amine Beihaqi <abeihaqi@e1r2p4.1337.ma>
2023-08-03 12:19:13 +0100

    no verbose

--- CPP_Module_04/ex03/AMateria.cpp
+++ CPP_Module_04/ex03/AMateria.cpp
@@ -2,29 +2,29 @@
 
 AMateria::AMateria( void ): type("Undefined")
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl ;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl ;
 }
 
 AMateria::AMateria( const AMateria& rhs ): type(rhs.type)
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl ;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl ;
 }
 
 AMateria& AMateria::operator=( const AMateria& rhs )
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl ;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl ;
     (void)rhs ;
     return (*this) ;
 }
 
 AMateria::~AMateria( void )
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl ;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl ;
 }
 
 AMateria::AMateria( const std::string& type ): type(type)
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl ;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl ;
 }
 
 const std::string& AMateria::getType( void ) const //Returns the materia type
@@ -35,6 +35,6 @@ const std::string& AMateria::getType( void ) const //Returns the materia type
 
 void AMateria::use( ICharacter& target )
 {
-    std::cout << __PRETTY_FUNCTION__ << std::endl ;
+    // std::cout << __PRETTY_FUNCTION__ << std::endl ;
     (void)target ;
 }
